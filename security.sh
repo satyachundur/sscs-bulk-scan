b3978c4559868fc5643ddf0de41ccb6cf2670370
bkpatel81 <66790434+bkpatel81@users.noreply.github.com>
2020-07-24 12:39:49 +0100

    updated alert to High and reporting config

--- security.sh
+++ security.sh
@@ -6,4 +6,7 @@ zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 echo "listings of zap folder"
 ls -la /zap
 cp /zap/api-report.html functional-output/
-zap-cli -p 1001 alerts -l Informational
+zap-cli -p 1001 alerts -l High
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+cp *.* functional-output/
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
